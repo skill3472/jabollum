89282e2c0c8175742700dd3f64572d7bf1060041
Maksymilian Tym <tymszczur@gmail.com>
2023-03-27 12:44:25 +0200

    Added a simple CLI for adding to the database

---
+++ db_cli.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+file = "db/db.json"
+
+def readfile(file):
+    if os.path.exists(file) and os.stat(file).st_size > 0:
+        with open(file, "r") as f:
+            return json.load(f)
+    else:
+        return []
+
+def appendfile(file, data):
+    temp = readfile(file)
+    temp.append(data)
+    with open(file, "w") as f:
+        json.dump(temp, f, indent=4)
+
+def addentry(file):
+    data = {}
+    data["name"] = input("Podaj nazwe napoju: ")
+    data["shops"] = input("Podaj sklepy w ktorych mozna kupic napoj, oddzielone przecinkiem i spacja: ")
+    data["score"] = input("Podaj swoja ocene napoju, od 1 do 10: ")
+    verified = input("Podaj status weryfikacji (Y/N): ")
+    if(verified == "Y"):
+        data["verified"] = True
+    else:
+        data["verified"] = False
+    appendfile(file, data)
+
+addentry(file)
