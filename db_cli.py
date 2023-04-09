ab96be4f7f5047a64e86526b7800b602e83cd3ac
Maksymilian Tym <tymszczur@gmail.com>
2023-04-09 15:04:23 +0200

    Updated the CLI for the rating system

--- db_cli.py
+++ db_cli.py
@@ -22,6 +22,8 @@ def addentry(file):
     data["name"] = input("Podaj nazwe napoju: ")
     data["shops"] = input("Podaj sklepy w ktorych mozna kupic napoj, oddzielone przecinkiem i spacja: ")
     data["score"] = int(input("Podaj swoja ocene napoju, od 1 do 10: "))
+    data["scores"] = []
+    data["votes"] = []
     verified = input("Podaj status weryfikacji (Y/N): ")
     if(verified == "Y"):
         data["verified"] = True
